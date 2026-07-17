86e84436c1c0404223df6774b3c8f4291d4a729f
Omar husien <139303831+Omarhusien072@users.noreply.github.com>
2026-07-17 23:32:02 +0300

    Fix homepage URL to .io

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    devIndicators: false
+    output: "export",
+    images: {
+        unoptimized: true,  
+    },
+    basePath: "/Portfolio",
 };
 
 export default nextConfig;
