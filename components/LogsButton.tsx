da001f63212468a6abb5ce397b70e6d212d8dad5
Omar husien <139303831+Omarhusien072@users.noreply.github.com>
2026-07-18 08:27:24 +0300

    Made the logs button hidden on phones + removed all the media query md sm from all the sub elements of the logs button

--- components/LogsButton.tsx
+++ components/LogsButton.tsx
@@ -14,11 +14,11 @@ function LogsButton({logs}: LogsEntry){
 
     return(
         <>
-        <div className="fixed xl:hidden top-0 right-0 mr-0.5 sm:mr-2 md:mr-4 z-50">
+        <div className="fixed hidden md:block xl:hidden top-0 right-0 mr-0.5 sm:mr-2 md:mr-4 z-50">
             <button className="flex justify-between items-center bg-secondary-background gap-[0.05rem] md:gap-[0.2rem] p-1 md:px-2 md:py-2 mt-4 outline-none border 
             border-primary/30 cursor-pointer rounded-sm hover:border-primary/70 transition-all duration-300"
             onClick={() => {setShowOverlay(true)}}>
-                <SystemLogsIcon className="size-[0.4rem] sm:size-4 text-primary"/>
+                <SystemLogsIcon className="size-[0.4rem] sm:size-[0.4rem] md:size-4 text-primary"/>
                 <span className='hidden md:block text-[0.6rem] md:text-[1rem] text-muted-foreground/90'>log</span>
                 {logs === undefined || logs.length !== 0 && <span className='text-[0.4rem] md:text-[1rem] text-primary'>{logs.length > 99 ? '99+' : logs.length}</span>}
             </button>
